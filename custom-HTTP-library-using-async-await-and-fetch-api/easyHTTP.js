77a725b7eb62991cfe8d8004f7052c4a118fe8b0
parmeet <parmeet.asi@gmail.com>
2018-10-02 18:50:07 +0530

    added async await snippets using fetch api

---
+++ custom-HTTP-library-using-async-await-and-fetch-api/easyHTTP.js
@@ -0,0 +1,40 @@
+class EasyHTTP {
+  //get endpoint
+  async get(url) {
+    const response = await fetch(url);
+    const resData = await response.json();
+    return resData;
+  }
+
+  //post endpoint
+  async post(url, data) {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: { "Content-type": "application/json" },
+      body: JSON.stringify(data)
+    });
+    const resData = await response.json();
+    return resData;
+  }
+
+  //put endpoint
+  async put(url, data) {
+    const response = await fetch(url, {
+      method: "PUT",
+      headers: { "Content-type": "application/json" },
+      body: JSON.stringify(data)
+    });
+    const resData = await response.json();
+    return resData;
+  }
+
+  //delete endpoint
+  async delete(url) {
+    const response = await fetch(url, {
+      method: "DELETE",
+      headers: { "Content-type": "application/json" }
+    });
+    const resData = await "Resource deleted";
+    return resData;
+  }
+}
